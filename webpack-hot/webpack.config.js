924ed31e6c208d41eecba83ade94379878611ef3
matus dubrava <matusdubrava@gmail.com>
2018-07-26 08:14:36 +0200

    hot module replacement webpack

---
+++ webpack-hot/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './public/js/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/assets/',
+    open: true,
+    openPage: '/views/index.html'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader', 'style-loader']
+      }
+    ]
+  }
+};
